2d1e6ef468c3da20a601a7033db107559e45863f
yiyi1026 <vivian.li.uibe@gmail.com>
2017-01-07 16:40:52 -0800

    p3,p5,p6

--- p1-p50/p3.rb
+++ p1-p50/p3.rb
@@ -14,11 +14,11 @@ end
 p largest_prime_factor(600851475143)
 
 #Solution2
-require 'prime'
-def largest_prime_factor(num)
-  
-  
-  
-  
-end
+# require 'prime'
+# def largest_prime_factor(num)
+#
+#
+#
+#
+# end
 
