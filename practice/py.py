dd47ddb181b0e7ea412e425339c90038187706ef
yiyi1026 <vivian.li.uibe@gmail.com>
2017-08-31 20:37:56 -0700

    python practice

---
+++ practice/py.py
@@ -0,0 +1,18 @@
+def median(lst):
+  lst.sort()
+  # print 'list', lst
+  length = len(lst)
+  if length % 2 == 0 :
+    right = length / 2
+    sum = lst[right] + lst[right-1]
+    if sum % 2 == 0:
+      return sum/2
+    else:
+      return sum/2.0
+  elif length == 1:
+  	return lst[0]
+  else :
+    mid = length/2
+    return lst[mid]
+
+print median([6,8,12,2,23])
