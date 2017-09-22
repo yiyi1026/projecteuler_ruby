879488871d22399b61da847a724f8ac4e24dcefd
yiyi1026 <vivian.li.uibe@gmail.com>
2017-09-22 14:34:00 -0700

    commit

--- codefights/test.py
+++ codefights/test.py
@@ -1,14 +1,34 @@
 # def multiplicationTable(n):
 #     return list(map(lambda x: range(x,x*(n+1),x), range(1,n+1)))
+from collections import deque
 
 # print multiplicationTable(5)
 
 # print sorted(set(map(lambda x: x, 'abc EFD')))
-a = []
-b = [5,2,1,4]
-c = [5,2,3,1,4]
+# a = []
+# b = [5,2,1,4]
+digits = [5,2,3,1,4]
 # print b < a and b < a
-print set(a) < set(b)
-print a < b
-print b < c
+# print set(a) < set(b)
+# print a < b
+# print b < c
+# print c[2:]
+# print c[:2]
+# print c[2:] + c[:2]
+n = 5
+# res = [deque(digits) for _ in range(n)]
+# map(lambda x: res[x].rotate(n-x),range(n))
+# print res
 
+from itertools import dropwhile
+
+def memoryPills(pills):
+    gen = dropwhile(lambda x: len(x) % 2 > 0, pills)
+    next(gen)
+    # return next(gen)
+    return [next(gen) for _ in range(3)]
+    # return gen
+    # return [next(gen) for _ in range(3)]
+# print memoryPills(['abc','abcd','abx', 'ab'])
+
+print int('3')
--- codefights/test.rb
+++ codefights/test.rb
@@ -1,5 +1,45 @@
-def multiplicationTable(n)
-  return (1..n).map{|el|(1..n).map{|x|x*el}}
+# def multiplicationTable(n)
+#   return (1..n).map{|el|(1..n).map{|x|x*el}}
+# end
+
+# p multiplicationTable(5)
+
+def solve(year)
+  # Complete this function
+  ((year % 400 == 0) || (year % 4 == 0 && (year % 100 != 0)) ) ?  "12.09.#{year}" : "13.09.#{year}"
 end
 
-p multiplicationTable(5)
+# p solve(1700)
+def russianRoulette(cylinder, startingCylinder, startingPlayer, players)
+  return players if players.length == 1
+  cylinder = cylinder.rotate(startingCylinder)
+  j = players.index(startingPlayer)
+  i = 0
+  while i < cylinder.length && players.length > 1
+    j %= players.length if j >= players.length
+    result = cylinder[i]
+    i += 1
+    if result == 'b'
+        # p j
+        # p players
+        players = players[0...j] + players[j+1..-1]
+        next
+    end
+    p 'here'
+    j += 1
+    # p i
+      
+  end
+  players
+      
+  
+end
+
+cylinder = ["b", "e", "e", "e", "e", "b", "b"]
+startingCylinder = 0
+startingPlayer = "kenny"
+# players = ["bebe", "eric", "stan", "kenny"]
+# p russianRoulette(cylinder, startingCylinder, startingPlayer, players)
+# p cylinder[1..-1] 
+a = ['a', 'b']
+p a[1] == 'b'
