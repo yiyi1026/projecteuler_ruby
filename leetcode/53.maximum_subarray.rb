92d738f391d1aa31358825e60865b7e0a47359eb
yiyi1026 <vivian.li.uibe@gmail.com>
2017-09-26 13:03:25 -0700

    initial commit leetcode 53

---
+++ leetcode/53.maximum_subarray.rb
@@ -0,0 +1,19 @@
+class Solution(object):
+  def maxSubArray(self, nums):
+    # 1. O(nlgn) 
+    # divide and conquer
+    # the largest sum contiguous subarray is either in the left half side of array, or right side, or (part of left and part of right)
+
+
+    # 2. O(n)
+    # max_ending_here is the largest sum of subarray up to this position
+    # initialize max_ending_here and max_so_far as the first element of the array
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    for num in nums[1:]:
+      max_ending_here = max(nums, nums + max_ending_here)
+      max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+
+    
