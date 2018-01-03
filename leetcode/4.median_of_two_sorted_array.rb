9ae2c0f63a38e5f34b792acc8bd69b39f5b9a5b5
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-03 15:48:21 -0800

    add 4,5,8, leetcode

--- leetcode/4.median_of_two_sorted_array.rb
+++ leetcode/4.median_of_two_sorted_array.rb
@@ -1,3 +1,19 @@
+'''
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+'''
 def med(sorted, double, len)
   double ? (sorted[len] + sorted[len-1]) / 2.0 : sorted[len]
 end
@@ -20,6 +36,7 @@ def find_median_sorted_arrays(left, right)
   sorted += left + right
   med(sorted, double, len/2) 
 end
+
 arr1 = [1,2]
 arr2 = [3,4]
 p find_median_sorted_arrays(arr1, arr2)
--- leetcode/5.longest_palindromic_substring.rb
+++ leetcode/5.longest_palindromic_substring.rb
@@ -1,44 +1,107 @@
-def longest_palindrome(str)
-  # dynamic programming
-  n = str.length
-  return nil unless n > 0
-  return str if str == str.reverse
-  table = Array.new(n){Array.new(n)}
-  maxlen = 1
-  for i in 0...n
-    table[i][i] = true
-    maxlen = 1
-  end
+'''
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
-  for i in 0...n-1
-    if str[i] == str[i+1]
-      table[i][i+1] = true
-      start = i # start is the start index of longest palindrome substring
-      maxlen = 2
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+'''
+# def longest_palindrome(s)
+#   # dynamic programming
+#   n = s.length
+#   # case 1
+#   return nil unless n > 0
+#   # case 2
+#   return s if s == s.reverse
+    
+#   # case 3
+#   table = Array.new(n){Array.new(n)}
+#   maxlen = 1
+#   # all single chars are palindrome
+  
+#   for i in 0...n
+#     table[i][i] = true
+#     maxlen = 1
+#   end
+#   start = -1
+#   # all continuous same chars are palindrome
+#   for i in 0...n-1
+#     if s[i] == s[i+1]
+#       table[i][i+1] = true
+#       start = i # start is the start index of longest palindrome substring
+#       maxlen = 2
+#     end
+#   end
+
+#   # case 4
+#   k = 3  # current length of substring
+
+#   while k <= n
+#     i = 0
+#     while i + k - 1 < n
+#       j = i + k - 1 # end index of current substring
+#       if table[i+1][j-1] == true && s[i] == s[j]
+#         table[i][j] = true
+        
+#         if k > maxlen
+#           start = i
+#           maxlen = k
+#         end
+#       end
+#       i += 1
+#     end
+#     k += 1
+#   end
+
+#   s[start...start+maxlen]
+# end
+def longest_palindrome(str)
+  len = str.length
+  return nil if len == 0
+
+  table = Array.new(len){Array.new(len)}
+  max_len = 1
+
+  # single char
+  len.times do |i|
+    table[i][i] = true
+    max_len = 1
+  end
+  
+  start_idx = 0
+  # 2 continuous same chars
+  (len-1).times do |j|
+    if str[j] == str[j+1]
+      table[j][j+1] = true
+      start_idx = j
+      max_len = 2
     end
   end
 
-  k = 3  # current length of substring
-
-  while k <= n
-    i = 0
-    while i + k - 1 < n
-      j = i + k - 1 # end index of current substring
-      if table[i+1][j-1] == true && str[i] == str[j]
+  k = 3 # current length of substring
+  while k <= len
+    i= 0
+    while i + k - 1 < len
+      j = i + k - 1
+      if table[i+1][j-1] = true && str[i] == str[j]
         table[i][j] = true
-        
-        if k > maxlen
-          start = i
-          maxlen = k
+        if k > max_len
+          start_idx = i
+          max_len = k
         end
       end
-
       i += 1
     end
     k += 1
   end
-
-  str[start...start+maxlen]
+  max_len
 end
-
-p longest_palindrome('')
+p longest_palindrome('ababbabc')
