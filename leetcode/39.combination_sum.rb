fa4001d812f21f3f7a5877b7b2c158da4546816e
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-05 21:53:31 -0800

    add leetcode 39 69

---
+++ leetcode/39.combination_sum.rb
@@ -0,0 +1,29 @@
+'''
+Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+'''
+@ret = []
+def combination_sum(candidates, target, idx = 0, selected=[], currentsum=0)
+  if currentsum >= target
+    @ret << selected if currentsum == target
+    return 
+  end
+  candidates[idx..-1].each_with_index do |c,id|
+    combination_sum(candidates, target, id+idx, selected + [c], currentsum + c)
+  end
+
+end
+
+combination_sum([2,3,6,7], 7)
+p @ret
--- leetcode/69.sqrtx.rb
+++ leetcode/69.sqrtx.rb
@@ -18,7 +18,7 @@ Explanation: The square root of 8 is 2.82842..., and since we want to return an 
 
 '''
 
-def square_root(x)
+def sqrt (x)
   return x if x == 0 || x == 1
   i = 1
   until (i+1)**2 > x
@@ -26,7 +26,49 @@ def square_root(x)
   end
 
   i
-
 end
 
-p square_root(2)
+def sqrt1(x)
+  cut = x.to_s.chars
+  if cut.length % 2 > 0
+    remainder = cut[0].to_i
+    cut.shift
+    result=''
+  else
+    remainder = (cut[0] + cut[1]).to_i
+    cut = cut[2..-1]
+    result=''
+
+  end    
+  (9.downto(1)).each do |i|
+    if i**2 <= remainder
+      result += i.to_s
+      break
+    end
+  end
+
+  remainder = (remainder - result.to_i**2)
+  mid = 2*10*(result.to_i)
+
+  while cut.length > 0
+
+    cut << '0' if cut.length == 1
+    remainder = (remainder.to_s + cut[0..1].join("")).to_i
+    cut = cut[2..-1]
+    temp = 0
+    temp += 1 until (mid+temp)*temp > remainder
+    if temp > 0
+      temp -= 1
+      result += temp.to_s
+    else
+      temp = 0
+      result += 0.to_s
+    end
+    remainder -= (mid+temp)*temp
+    mid += temp*2
+    mid = mid * 10
+  end
+  result.to_i
+end
+
+p sqrt1(2147302921)
