cf3b03e2a28052810de267677cd2a46d87af5912
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-15 19:09:28 -0800

    add leetcode 82

---
+++ leetcode/82.remove_duplicates_from_sorted_list_ii.rb
@@ -0,0 +1,72 @@
+'''
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+'''
+
+class ListNode
+	attr_accessor :val, :next
+	def initialize(val)
+			@val = val
+			@next = nil
+	end
+end
+
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+  return head if head.nil? || head.next.nil?
+	solution = ListNode.new(nil)
+	solution_acc = solution
+	head_acc = head
+	current_acc = head_acc
+	head_acc = head_acc.next
+  repeat = false
+	until head_acc.nil?
+		
+		if head_acc.val == current_acc.val
+			until head_acc.nil? || head_acc.val != current_acc.val
+				head_acc = head_acc.next
+			end
+			# p head_acc
+			if head_acc.nil?
+				current_acc = nil
+				break
+			end
+			if head_acc.next.nil?
+				solution_acc.next = head_acc
+				solution_acc = solution_acc.next
+				current_acc = nil
+				break
+			else
+				current_acc = head_acc
+				head_acc = head_acc.next
+			end
+		else
+			solution_acc.next = current_acc
+			current_acc = head_acc
+			solution_acc = solution_acc.next
+			head_acc = head_acc.next
+		end
+	end
+	solution_acc.next = current_acc
+	current_acc.next = nil unless current_acc.nil?
+	solution.next
+end
+
+arr = [1]
+head = ListNode.new(1)
+start = head
+arr.each do |val|
+  start.next = ListNode.new(val)
+  start = start.next
+end
+# start.next = ListNode.new(start.val)
+# start = start.next
+# start.next = ListNode.new(start.val)
+
+p head
+
+p delete_duplicates(head)
