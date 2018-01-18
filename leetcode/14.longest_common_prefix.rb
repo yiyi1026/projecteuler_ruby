125c8da316c682e00e273ba68079225d91add7d6
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-17 22:35:51 -0800

    leetcode refactor 14

--- leetcode/14.longest_common_prefix.rb
+++ leetcode/14.longest_common_prefix.rb
@@ -2,7 +2,7 @@
   Write a function to find the longest common prefix string amongst an array of strings.
 '''
 
-def longest_common_prefix(strs) # leetcode 14
+def longest_common_prefix(strs)
   return '' if strs.empty?
   
   prefix = strs[0]
@@ -13,6 +13,22 @@ def longest_common_prefix(strs) # leetcode 14
   end
  
   prefix.slice(0,cnt)
+end
+
+# revised
+def longest_common_prefix(strs)
+  return '' if strs.empty?
+  prefix = strs[0]
+  len = prefix.length
+  return '' if len == 0
+  i = len-1
+  comp = strs[1..-1]
+  comp.each do |str|
+    i -= 1 until str[i] == prefix[i] || i < 0
+    i -= 1 until str[0..i] == prefix[0..i] || i < 0
+    break if i < 0
+  end
+  i >= 0 ? prefix[0..i] : ''
 end
 
 p longest_common_prefix(['flower', 'flow', 'flick'])
