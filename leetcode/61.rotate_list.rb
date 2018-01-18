5f2ae38ef1f15259830cc9fd85cfb5e02402457d
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-18 13:44:05 -0800

    add leetcode 61

--- leetcode/61.rotate_list.rb
+++ leetcode/61.rotate_list.rb
@@ -49,6 +49,30 @@ def rotate_right(head, k)
   sol
 end
 
+def rotate_right(head, k)
+  return head if head.nil? || head.next.nil? || k == 0
+  dummy = ListNode.new(nil)
+  dummy.next = head
+  slow = dummy
+  fast = head
+
+  ((k-1) % head.length).times do |_|
+    fast = fast.next
+  end
+
+  until fast.next.nil?
+    slow = slow.next
+    fast = fast.next
+  end
+
+  new_head = ListNode.new(nil)
+  new_head.next = slow.next
+  slow.next = nil
+  fast.next = dummy.next
+
+  new_head.next
+end
+
 head = ListNode.new(1)
 head.next = ListNode.new(2)
 two = head.next
@@ -56,4 +80,4 @@ two.next = ListNode.new(3)
 two = two.next
 two.next = ListNode.new(4)
 p head
-p rotate_right(head,1)
+p rotate_right(head,2)
