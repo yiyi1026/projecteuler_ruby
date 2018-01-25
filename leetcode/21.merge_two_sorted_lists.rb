b590d2f4b91084430cd1e659cff769c0604a67b5
yiyi1026 <vivian.li.uibe@gmail.com>
2018-01-25 12:38:54 -0800

    add leetcode 21

--- leetcode/21.merge_two_sorted_lists.rb
+++ leetcode/21.merge_two_sorted_lists.rb
@@ -13,29 +13,53 @@ class ListNode
     end
 end
 
+# def merge_two_lists(l1, l2)
+#     new = []
+#     while l1 && l2
+#         case l1.val <=> l2.val
+#         when 1
+#             new << l2.val
+#             l2 = l2.next
+#         else
+#             new << l1.val
+#             l1 = l1.next
+#         end
+#     end
+#     if l1
+#         until l1.nil?
+#             new << l1.val
+#             l1 = l1.next
+#         end
+#     end
+#     if l2
+#         until l2.nil?
+#             new << l2.val
+#             l2 = l2.next
+#         end
+#     end
+#     new
+# end
+
+# revised
 def merge_two_lists(l1, l2)
-    new = []
-    while l1 && l2
-        case l1.val <=> l2.val
-        when 1
-            new << l2.val
-            l2 = l2.next
-        else
-            new << l1.val
-            l1 = l1.next
-        end
-    end
-    if l1
-        until l1.nil?
-            new << l1.val
-            l1 = l1.next
-        end
-    end
-    if l2
-        until l2.nil?
-            new << l2.val
-            l2 = l2.next
-        end
-    end
-    new
+	dummy = ListNode.new(nil)
+	acc = dummy
+	while l1 && l2
+		case l1.val <=> l2.val
+		when 1
+			acc.next = l2
+			l2 = l2.next
+		else
+			acc.next = l1
+			l1 = l1.next																		 
+		end
+		acc = acc.next
+	end
+	if l1
+		acc.next = l1
+	elsif l2
+		acc.next = l2
+	end
+
+	dummy.next
 end
