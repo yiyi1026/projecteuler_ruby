0a84bfbd074947aa016879aab0f10f3561727dbc
yiyi1026 <vivian.li.uibe@gmail.com>
2018-02-04 21:59:53 -0800

    complement 404 description

--- leetcode/404.sum_of_left_leaves.rb
+++ leetcode/404.sum_of_left_leaves.rb
@@ -1,4 +1,16 @@
+'''
+Find the sum of all left leaves in a given binary tree.
 
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+'''
 
 
 # Definition for a binary tree node.
@@ -18,6 +30,5 @@ def sum_of_left_leaves(root, left = false)
   elsif root.left.nil? && root.right.nil?
     return left ? root.val : 0
   else
-    return sum_of_left_leaves(root.left, true) + sum_of_left_leaves(root.right, false)
   end
 end
