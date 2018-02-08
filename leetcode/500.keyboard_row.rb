2ecdc68e3d3a1f377161bdbd0af2f86ef0359a44
yiyi1026 <vivian.li.uibe@gmail.com>
2018-02-08 14:41:07 -0800

    add leetcode 500

---
+++ leetcode/500.keyboard_row.rb
@@ -0,0 +1,34 @@
+'''
+Given a List of words, return the words that can be typed using letters of alphabet on only one row\'s of American keyboard like the image below.
+
+
+American keyboard
+
+
+Example 1:
+Input: ["Hello", "Alaska", "Dad", "Peace"]
+Output: ["Alaska", "Dad"]
+'''
+
+# @param {String[]} words
+# @return {String[]}
+def row_word?(word)
+  row_hash={}
+  'qwertyuiop'.each_char{|ch|row_hash[ch]=1}
+  'asdfghjkl'.each_char{|ch|row_hash[ch]=2}
+  'zxcvbnm'.each_char{|ch|row_hash[ch]=3}
+  
+  word_split = word.chars
+  standard = nil
+  word_split.each do |ch|
+    if standard.nil?
+      standard = row_hash[ch]
+    else
+      return false unless standard == row_hash[ch]
+    end
+  end
+  true
+end
+def find_words(words)
+  words.select{|word|row_word?(word.downcase)}
+end
